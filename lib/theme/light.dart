5e67459d2e5e717f48854d778ec9ab66f6e6308b
montybytes <muturimonty@gmail.com>
2026-01-27 13:35:16 +0300

    clean up unused theme data

--- lib/theme/light.dart
+++ lib/theme/light.dart
@@ -4,66 +4,20 @@ import "package:nxcalculator/theme/constants.dart";
 final lightTheme = ThemeData(
   useMaterial3: true,
   brightness: Brightness.light,
+  canvasColor: lightThemeBackground,
   scaffoldBackgroundColor: lightThemeBackground,
   appBarTheme: appBarDefaultTheme.copyWith(
-    iconTheme: const IconThemeData(color: lightThemeText),
     backgroundColor: lightThemeBackground,
     foregroundColor: lightThemeText,
   ),
+  textSelectionTheme: textSelectionDefaultTheme,
   textTheme: ThemeData().textTheme.apply(
+    fontFamily: defaultFontFamily,
     bodyColor: lightThemeText,
     displayColor: lightThemeText,
   ),
   cardTheme: cardDefaultTheme.copyWith(color: lightThemeCard),
-  floatingActionButtonTheme: fabDefaultTheme,
-  elevatedButtonTheme: buttonDefaultTheme,
-  iconTheme: const IconThemeData(color: lightThemeText),
-  listTileTheme: listTileDefaultTheme.copyWith(
-    textColor: lightThemeText,
-    tileColor: lightThemeListItem,
-  ),
-  expansionTileTheme: expansionTileDefaultTheme.copyWith(
-    backgroundColor: lightThemeListItem,
-    textColor: lightThemeText,
-    iconColor: lightThemeText,
-    collapsedBackgroundColor: lightThemeListItem,
-    collapsedTextColor: lightThemeText,
-    collapsedIconColor: lightThemeText,
-  ),
-  checkboxTheme: checkboxDefaultTheme,
-  switchTheme: switchDefaultTheme,
-  textButtonTheme: textButtonDefaultTheme,
   dialogTheme: const DialogThemeData(backgroundColor: lightThemeBackground),
-  bottomSheetTheme: const BottomSheetThemeData(
-    backgroundColor: lightThemeBackground,
-  ),
-  inputDecorationTheme: textInputDefaultDecoration.copyWith(
-    fillColor: lightThemeListItem,
-  ),
+  bottomSheetTheme: const BottomSheetThemeData(backgroundColor: lightThemeCard),
   dividerColor: lightThemeText,
-  timePickerTheme: timePickerDefaultTheme.copyWith(
-    dialHandColor: nothingRed,
-    dialTextColor: lightThemeText,
-    dialBackgroundColor: lightThemeListItem,
-    dayPeriodColor: nothingRed,
-    dayPeriodTextColor: lightThemeText,
-    hourMinuteColor: lightThemeListItem,
-    hourMinuteTextColor: lightThemeText,
-    backgroundColor: lightThemeBackground,
-    dayPeriodBorderSide: const BorderSide(color: lightThemeListItem),
-    inputDecorationTheme: textInputDefaultDecoration.copyWith(
-      fillColor: lightThemeListItem,
-      contentPadding: EdgeInsets.zero,
-    ),
-  ),
-  textSelectionTheme: textSelectionDefaultTheme,
-  chipTheme: chipDefaultTheme,
-  iconButtonTheme: IconButtonThemeData(
-    style: ButtonStyle(
-      iconColor: WidgetStateColor.resolveWith((states) {
-        return lightThemeText;
-      }),
-    ),
-  ),
-  canvasColor: lightThemeBackground,
 );
